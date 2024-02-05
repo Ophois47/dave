bc9023c1acde3b8c724b7247019eaba83019e9ea
Ophois47 <dpetnick89@gmail.com>
2024-02-04 21:26:47 -0500

    Implemented progress spinner.

--- src/utils.rs
+++ src/utils.rs
@@ -2,20 +2,24 @@ use std::io;
 use std::fs;
 use std::path::Path;
 use bytesize::ByteSize;
+use spinners::{Spinner, Spinners};
 use walkdir::WalkDir;
 
 pub fn get_file_size(path: &Path) -> io::Result<()> {
-    println!("Your Path is '{}'", path.display());
-
     let file_metadata = fs::metadata(path)?;
+
     if file_metadata.is_dir() {
-        println!("Passed Path Points to a Directory.");
-        println!("Exploring Contents ...\n");
         let mut files_read = 0;
-        for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-            println!("{}", entry.path().display());
+
+        println!("##==> Path '{}' Points to a Directory.", path.display());
+        let mut spinner = Spinner::new(Spinners::SimpleDotsScrolling, "Calculating Size".into());
+
+        for _entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+            // println!("{}", entry.path().display());
             files_read += 1;
         }
+        spinner.stop();
+
         let total_size = WalkDir::new(path)
             .min_depth(1)
             .max_depth(100)
@@ -25,19 +29,19 @@ pub fn get_file_size(path: &Path) -> io::Result<()> {
             .filter(|metadata| metadata.is_file())
             .fold(0, |acc, m| acc + m.len());
 
-        println!("\n##==> Number of Files Read: {}", files_read);
-        println!("##==> Directory Size: {}", ByteSize::b(total_size));
+        println!("\n##==>> Number of Files Read: {}", files_read);
+        println!("##==>> Directory Size: {}", ByteSize::b(total_size));
     } else if file_metadata.is_file() {
-        println!("Passed Path Points to a File.");
-        println!("Calculating Size of File ...");
-        println!("Size of File: {}", ByteSize::b(file_metadata.len()));
+        println!("##==> Path '{}' Points to a File.", path.display());
+        println!("##==> Calculating Size of File ...");
+        println!("##==>> Size of File: {}", ByteSize::b(file_metadata.len()));
     } else {
-        println!("Idk wtf that is ...");
+        println!("##==>>> Warning! Idk wtf that is ...");
     }
     Ok(())
 }
 
 pub fn greeting(name: String) -> io::Result<()> {
-    println!("Hey {}!", name);
+    println!("##==> Hey {}!", name);
     Ok(())
 }
