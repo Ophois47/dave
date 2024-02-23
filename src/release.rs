db32dd0a0ccf1fcc9e7f8da1c9cc643751820a4f
david <davidp@cyber.com>
2024-02-23 09:31:45 -0500

    Added new PNG image to help visualize game map when locations change and new ones are added. Updated readme and several important comments, and added new items and polished the descriptions and information regarding game locations and objects.

--- src/release.rs
+++ src/release.rs
@@ -2,5 +2,5 @@ pub const BUILD_DATE: &str = env!("BUILD_DATE");
 pub const BINARY_NAME: &str = "dave";
 pub const DISPLAY_NAME: &str = "Dave";
 pub const DISPLAY_DESCRIPTION: &str = "I am Dave. I fucking rule so god damn hard.";
-pub const VERSION: [u8; 3] = [3, 6, 6];
-pub const VERSION_STR: &str = "3.6.6";
+pub const VERSION: [u8; 3] = [3, 8, 0];
+pub const VERSION_STR: &str = "3.8.0";
