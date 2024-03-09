3527c72cef63146e39a33448c12f90a537d2d4ca
Ophois47 <dpetnick89@gmail.com>
2024-03-08 22:49:10 -0500

    Included Swiss Franc with currency converter.

--- src/dave_currency.rs
+++ src/dave_currency.rs
@@ -17,6 +17,7 @@ fn match_from_currency(from_currency: &str, to_currency: &str, num: &f32) -> f32
 		"JPY" => convert_from_jpy(&to_currency, &num),
 		"CNY" => convert_from_cny(&to_currency, &num),
 		"AUD" => convert_from_aud(&to_currency, &num),
+		"CHF" => convert_from_chf(&to_currency, &num),
 		_ => 0.0,
 	}
 }
@@ -28,6 +29,7 @@ fn convert_from_usd(to_currency: &str, num: &f32) -> f32 {
 	let cad_to_usd_rate = 1.34905;
 	let cny_to_usd_rate = 7.19503;
 	let aud_to_usd_rate = 1.5151;
+	let chf_to_usd_rate = 0.87983;
 
 	let amount: f32 = match to_currency {
 		"EUR" => *num * eur_to_usd_rate,
@@ -36,6 +38,7 @@ fn convert_from_usd(to_currency: &str, num: &f32) -> f32 {
 		"JPY" => *num * jpy_to_usd_rate,
 		"CNY" => *num * cny_to_usd_rate,
 		"AUD" => *num * aud_to_usd_rate,
+		"CHF" => *num * chf_to_usd_rate,
 		"USD" => *num,
 		_ => 0.0,
 	};
@@ -50,6 +53,7 @@ fn convert_from_jpy(to_currency: &str, num: &f32) -> f32 {
 	let usd_to_jpy_rate = 0.00675;
 	let cny_to_jpy_rate = 0.04855;
 	let aud_to_jpy_rate = 0.01022;
+	let chf_to_jpy_rate = 0.00594;
 
 	let amount: f32 = match to_currency {
 		"USD" => *num * usd_to_jpy_rate,
@@ -58,6 +62,7 @@ fn convert_from_jpy(to_currency: &str, num: &f32) -> f32 {
 		"CAD" => *num * cad_to_jpy_rate,
 		"CNY" => *num * cny_to_jpy_rate,
 		"AUD" => *num * aud_to_jpy_rate,
+		"CHF" => *num * chf_to_jpy_rate,
 		"JPY" => *num,
 		_ => 0.0,
 	};
@@ -72,6 +77,7 @@ fn convert_from_eur(to_currency: &str, num: &f32) -> f32 {
     let cad_to_eur_rate = 1.47219;
     let cny_to_eur_rate = 7.85179;
     let aud_to_eur_rate = 1.65355;
+    let chf_to_eur_rate = 0.9602;
 
     let amount: f32 = match to_currency {
         "USD" => *num * usd_to_eur_rate,
@@ -80,6 +86,7 @@ fn convert_from_eur(to_currency: &str, num: &f32) -> f32 {
         "JPY" => *num * jpy_to_eur_rate,
         "CNY" => *num * cny_to_eur_rate,
         "AUD" => *num * aud_to_eur_rate,
+        "CHF" => *num * chf_to_eur_rate,
         "EUR" => *num,
         _ => 0.0,
     };
@@ -94,6 +101,7 @@ fn convert_from_gbp(to_currency: &str, num: &f32) -> f32 {
     let usd_to_gbp_rate = 1.27626;
     let cny_to_gdp_rate = 9.18275;
     let aud_to_gbp_rate = 1.93367;
+    let chf_to_gbp_rate = 1.12298;
 
     let amount: f32 = match to_currency {
         "USD" => *num * usd_to_gbp_rate,
@@ -102,6 +110,7 @@ fn convert_from_gbp(to_currency: &str, num: &f32) -> f32 {
         "JPY" => *num * jpy_to_gbp_rate,
         "CNY" => *num * cny_to_gdp_rate,
         "AUD" => *num * aud_to_gbp_rate,
+        "CHF" => *num * chf_to_gbp_rate,
         "GBP" => *num,
         _ => 0.0,
     };
@@ -116,6 +125,7 @@ fn convert_from_cad(to_currency: &str, num: &f32) -> f32 {
     let usd_to_cad_rate = 0.74116;
     let cny_to_cad_rate = 5.33268;
     let aud_to_cad_rate = 1.12293;
+    let chf_to_cad_rate = 0.6521;
 
     let amount: f32 = match to_currency {
         "USD" => *num * usd_to_cad_rate,
@@ -124,6 +134,7 @@ fn convert_from_cad(to_currency: &str, num: &f32) -> f32 {
         "JPY" => *num * jpy_to_cad_rate,
         "CNY" => *num * cny_to_cad_rate,
         "AUD" => *num * aud_to_cad_rate,
+        "CHF" => *num * chf_to_cad_rate,
         "CAD" => *num,
         _ => 0.0,
     };
@@ -138,6 +149,7 @@ fn convert_from_cny(to_currency: &str, num: &f32) -> f32 {
     let usd_to_cny_rate = 0.13895;
     let cad_to_cny_rate = 0.18745;
     let aud_to_cny_rate = 0.21052;
+    let chf_to_cny_rate = 0.12225;
 
     let amount: f32 = match to_currency {
         "USD" => *num * usd_to_cny_rate,
@@ -146,6 +158,7 @@ fn convert_from_cny(to_currency: &str, num: &f32) -> f32 {
         "JPY" => *num * jpy_to_cny_rate,
         "CAD" => *num * cad_to_cny_rate,
         "AUD" => *num * aud_to_cny_rate,
+        "CHF" => *num * chf_to_cny_rate,
         "CNY" => *num,
         _ => 0.0,
     };
@@ -160,6 +173,7 @@ fn convert_from_aud(to_currency: &str, num: &f32) -> f32 {
     let usd_to_aud_rate = 0.6599;
     let cad_to_aud_rate = 0.89023;
     let cny_to_aud_rate = 4.74799;
+    let chf_to_aud_rate = 0.5806;
 
     let amount: f32 = match to_currency {
         "USD" => *num * usd_to_aud_rate,
@@ -168,7 +182,32 @@ fn convert_from_aud(to_currency: &str, num: &f32) -> f32 {
         "JPY" => *num * jpy_to_aud_rate,
         "CAD" => *num * cad_to_aud_rate,
         "CNY" => *num * cny_to_aud_rate,
+        "CHF" => *num * chf_to_aud_rate,
         "AUD" => *num,
+        _ => 0.0,
+    };
+
+    amount
+}
+
+fn convert_from_chf(to_currency: &str, num: &f32) -> f32 {
+	let gbp_to_chf_rate = 0.89033;
+	let eur_to_chf_rate = 1.04125;
+	let jpy_to_chf_rate = 168.404;
+	let usd_to_chf_rate = 1.13638;
+	let cad_to_chf_rate = 1.53302;
+	let aud_to_chf_rate = 1.72172;
+	let cny_to_chf_rate = 8.17625;
+
+	let amount: f32 = match to_currency {
+        "USD" => *num * usd_to_chf_rate,
+        "EUR" => *num * eur_to_chf_rate,
+        "GBP" => *num * gbp_to_chf_rate,
+        "JPY" => *num * jpy_to_chf_rate,
+        "CAD" => *num * cad_to_chf_rate,
+        "CNY" => *num * cny_to_chf_rate,
+        "AUD" => *num * aud_to_chf_rate,
+        "CHF" => *num,
         _ => 0.0,
     };
 
@@ -279,6 +318,22 @@ pub fn dave_currency_conv(
 		},
 		"AUD" => {
 			match Currency::from_code("AUD") {
+				Some(currency_code) => {
+					println!("##==> {} {}", amount, currency_code.name());
+					println!("##==> {}{}", currency_code.symbol(), amount);
+					println!("##==> The {} is in use by:", currency_code.name());
+					currency_code.used_by().iter().fold(true, |first, nation| {
+						if !first { print!(", "); }
+						print!("{}", nation.name());
+						false
+					});
+					println!("\n##==> Converting to {} ...", to_currency);
+				},
+				_ => {},
+			}
+		},
+		"CHF" => {
+			match Currency::from_code("CHF") {
 				Some(currency_code) => {
 					println!("##==> {} {}", amount, currency_code.name());
 					println!("##==> {}{}", currency_code.symbol(), amount);
