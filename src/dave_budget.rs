7136e0f810036c1a74ebe2109555207e1f7b7f44
Ophois47 <dpetnick89@gmail.com>
2024-03-18 20:31:17 -0400

    Changes to budget program.

--- src/dave_budget.rs
+++ src/dave_budget.rs
@@ -16,16 +16,12 @@ impl DaveBudget {
 	}
 
 	pub fn add_income(&mut self, amount: f64) {
-		self.income += amount;
+		self.income += amount
 	}
 
 	pub fn add_expense(&mut self, expense_name: String, amount: f64) {
 		self.expenses.insert(expense_name, amount);
 	}
-
-	/*fn remove_expense(&mut self, expense_name: String, amount: f64) {
-		self.expenses.remove(&expense_name, amount);
-	}*/
 
 	pub fn get_balance(&self) -> f64 {
 		self.income - self.expenses.values().sum::<f64>()
